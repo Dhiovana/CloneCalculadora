64d50c3d8cae9b3cec2e5211e30917405d9817bf
Your Name <you@example.com>
2023-06-13 16:48:53 -0300

    adicionando funcao operadores

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 using namespace std;
 
+int calcula(int n1, int n2, int calcOperador, int &i){
+    int resultado;
+    switch(calcOperador){
+        case '+': resultado = n1 + n2;
+        break;
+        case '-': resultado = n1 - n2;
+        break;
+        case '*': resultado = n1 * n2;
+        break;
+        case '/': resultado = n1/n2;
+        break;
+        default:
+        i = 0;
+        return 0;
+    }
+    i = 1;
+    return resultado;
+}
+
 int main(){
 
 	int numOne, numTwo, i, resultadoCalculo;
@@ -10,5 +29,7 @@ int main(){
 	cin >> numOne;
 	cout << "Insira o segund numero:\n";
 	cin >> numTwo;
+	cout << "Insira a operacao a ser realizada: (+ 'soma', -'subtracao', * 'multiplicacao' e / 'divisao')\n";
+	cin >> operador;
 
 }
