64a13756f6dd7aa9d66ea92b547ea75684ba31ff
Dhiovana <dhiovana.moreira@aluno.cefet-rj.br>
2023-06-13 16:31:23 -0300

    lendo os dois números

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+
+	int numOne, numTwo, i, resultadoCalculo;
+	char operador;
+	cout << "Insira o primeiro numero:\n";
+	cin >> numOne;
+	cout << "Insira o segund numero:\n";
+	cin >> numTwo;
+
+}
